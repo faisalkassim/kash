bd959726fc9c16881d25c309fc545d048284beda
Faisal <kassimfaisal5659@gmail.com>
2026-06-17 20:03:57 +0000

    Add basic REPL loop with exit command

--- UNIX SHELL/main.c
+++ UNIX SHELL/main.c
@@ -1,1 +1,29 @@
 // unix shell project
+
+#include <stdio.h>
+#include <string.h>
+
+#define INPUT_LENGTH 100
+
+int main()
+{
+    char user_input[INPUT_LENGTH + 1];
+
+    for (;;) //  prompts continuously
+    {
+        printf("kash> ");
+        fgets(user_input, INPUT_LENGTH, stdin);
+
+        // replace new line in user_input with \0
+        user_input[strcspn(user_input, "\n")] = '\0';
+
+        puts(user_input);
+        // program ends when user_input is "exit"
+
+        if (strcmp("exit", user_input) == 0)
+        {
+            break;
+        }
+    }
+    return 0;
+}
